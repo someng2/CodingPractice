1eb1c22bb3a9c5f4ee5c755b6b6c8cdd7ff00b2f
Somang Baek <96823668+someng2@users.noreply.github.com>
2022-08-22 01:30:15 +0900

    오픈채팅방

---
+++ Python/open_chatting_room.py
@@ -0,0 +1,34 @@
+import collections
+
+def solution(record):
+    result = []
+    user = {}
+    
+    for x in record:
+        temp = x.split()
+        message = temp[0]
+        uid = temp[1]
+        nick = ''
+        str1 = ''
+        if len(temp) == 3:
+            nick = temp[2]
+        # print(message, ',', uid, ',', nick)
+        
+        if message == "Enter" or message == "Change":
+            user[uid] = nick
+
+        # print('user = ', user)
+        
+    for x in record:
+        temp = x.split()
+        message = temp[0]
+        uid = temp[1]
+        
+        if message == "Enter":
+            result.append(user[uid] + "님이 들어왔습니다.")
+        elif message == "Leave":
+            result.append(user[uid] + "님이 나갔습니다.")
+            
+        # print('result = ', result, '\n')
+        
+    return result
