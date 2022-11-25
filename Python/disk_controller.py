4ba05f3f9b4843fcd4db325e5690cf8b52ee0a71
Somang Baek <96823668+someng2@users.noreply.github.com>
2022-11-25 22:21:17 +0900

    힙 <디스크 컨트롤러>

---
+++ Python/disk_controller.py
@@ -0,0 +1,26 @@
+import heapq
+
+def solution(jobs):
+    answer = 0
+    length = len(jobs)
+    heapq.heapify(jobs)
+    prev = -1
+    now = 0
+    i = 0
+    heap = []
+    
+    while i < len(jobs):
+        for j in jobs:
+            if prev < j[0] <= now:
+                heapq.heappush(heap, [j[1],j[0]])
+        
+        if heap:
+            duration2, start2 = heapq.heappop(heap)
+            prev = now
+            now += duration2
+            answer += (now - start2)
+            i += 1
+        else:
+            now += 1
+            
+    return int(answer/length)
