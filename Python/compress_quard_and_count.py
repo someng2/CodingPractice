8ff313d9c697f49cc0370539f237a4ac04289645
Somang Baek <96823668+someng2@users.noreply.github.com>
2023-02-13 21:43:07 +0900

    쿼드압축 후 개수 세기

---
+++ Python/compress_quard_and_count.py
@@ -0,0 +1,18 @@
+def solution(arr):
+    answer = [0, 0]
+                   
+    def compression(a,b,l):
+        start = arr[a][b]
+        for i in range(a,a+l):
+            for j in range(b, b+l):
+                if arr[i][j] != start:
+                    l = l//2
+                    compression(a,b,l)
+                    compression(a+l,b,l)
+                    compression(a,b+l,l)
+                    compression(a+l,b+l,l)
+                    return
+        answer[start] += 1
+
+    compression(0,0,len(arr))
+    return answer
